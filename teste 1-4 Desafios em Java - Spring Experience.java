23bf1b93ad68efabcae696a631a79934fb2c0fce
MateusDamasceno <mateus.damasceno@gmail.com>
2022-07-23 22:23:16 -0300

    Add files via upload

---
+++ teste 1-4 Desafios em Java - Spring Experience.java
@@ -0,0 +1,20 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+// TODO: complete os espaços em branco com sua solução para o problema
+		String[] nomes = new String[30];
+		
+		for(int i=0 ; i<10 ; i++)
+			nomes[i] = sc.nextLine();
+
+	
+	
+	 System.out.println(nomes[2]);
+	 System.out.println(nomes[6]);
+	 System.out.println(nomes[8]);
+	
+  }
+}
